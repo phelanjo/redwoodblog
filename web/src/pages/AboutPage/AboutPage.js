ba4c85b9df0fb7317c726ee9779c60b8b05fbfb4
Johnny Phelan <phelanjo@oregonstate.edu>
2020-03-21 16:00:39 -0700

    Make logo a route back to home page.

--- web/src/pages/AboutPage/AboutPage.js
+++ web/src/pages/AboutPage/AboutPage.js
@@ -8,7 +8,6 @@ const AboutPage = () => {
           This site was created to demonstrate my mastery of Redwood: Look on my
           works, ye mighty, and despair!
         </p>
-        <Link to={routes.home()}>Return home</Link>
       </BlogLayout>
   )
 }
